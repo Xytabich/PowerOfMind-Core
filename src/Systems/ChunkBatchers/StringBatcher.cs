ad10555e274b2c8562a411a2f4c0afd2eb33b0f1
Xytabich <xytabich@gmail.com>
2023-07-02 16:37:09 +0300

    string batcher simplification

--- src/Systems/ChunkBatchers/StringBatcher.cs
+++ src/Systems/ChunkBatchers/StringBatcher.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
-using Vintagestory.API.MathTools;
 
 namespace PowerOfMind.Systems.ChunkBatchers
 {
@@ -45,41 +44,28 @@ namespace PowerOfMind.Systems.ChunkBatchers
 			this.uniformStruct = uniformStruct;
 		}
 
-		public int AddString(int3 fromBlock, float3 fromOffset, int3 toBlock, float3 toOffset, IStringBuilder builder)
+		public int AddString(int3 fromBlock, int3 toBlock, IStringBuilder builder)
 		{
 			int stringChunksChain = -1;
 			int chunkSize = blockAccessor.ChunkSize;
 
 			int sid = strings.Add(new StringInfo(stringChunksChain, builder));
 
-			int3 prevBlock = fromBlock;
 			int3 prevChunk = fromBlock / chunkSize;
-
-			EnumerateLine(fromBlock.x, fromBlock.y, fromBlock.z, toBlock.x, toBlock.y, toBlock.z, (x, y, z) => {
-				var chunk = new int3(x / chunkSize, y / chunkSize, z / chunkSize);
-				if(chunk.Equals(prevChunk))
+			StringBatchUtil.EnumerateLine(fromBlock, toBlock, pos => {
+				var chunk = pos / chunkSize;
+				if(!chunk.Equals(prevChunk))
 				{
-					prevBlock = new int3(x, y, z);
-				}
-				else
-				{
-					var point = AddChunk(fromBlock, fromOffset, toBlock, toOffset, prevBlock, ref stringChunksChain, chunkSize, sid, prevChunk);
-
-					fromOffset = point - (chunk - prevChunk) * chunkSize;
-
-					prevBlock = fromBlock = new int3(x, y, z);
+					AddChunk(ref stringChunksChain, sid, prevChunk);
 					prevChunk = chunk;
 				}
 			});
-			AddChunk(fromBlock, fromOffset, toBlock, toOffset, prevBlock, ref stringChunksChain, chunkSize, sid, prevChunk);
+			AddChunk(ref stringChunksChain, sid, prevChunk);
 
 			return sid;
 
-			float3 AddChunk(int3 fromBlock, float3 fromOffset, int3 toBlock, float3 toOffset, int3 prevBlock, ref int stringChunksChain, int chunkSize, int sid, int3 chunk)
+			void AddChunk(ref int stringChunksChain, int sid, int3 chunk)
 			{
-				var fromPos = (fromBlock - chunk * chunkSize) + fromOffset;
-				var toPos = (toBlock - chunk * chunkSize) + toOffset;
-
 				int cid;
 				ChunkBatchGroup chunkBatch;
 				if(chunkToId.TryGetValue(chunk, out var chunkId))
@@ -97,11 +83,10 @@ namespace PowerOfMind.Systems.ChunkBatchers
 					mod.RegisterChunkDirtyListener(chunk, UpdateChunk);
 				}
 
-				cid = chunkStrings.Add(chunkBatch.stringChain, new ChunkStringSegment(chunkId, sid, fromPos, toPos, fromBlock, prevBlock));
+				cid = chunkStrings.Add(chunkBatch.stringChain, new ChunkStringSegment(chunkId, sid));
 				if(chunkBatch.stringChain < 0) chunkBatch.stringChain = cid;
 
 				stringChunksChain = stringChunks.Append(stringChunksChain, cid);
-				return toPos;
 			}
 		}
 
@@ -135,77 +120,6 @@ namespace PowerOfMind.Systems.ChunkBatchers
 			}
 		}
 
-		private static float3 FindRayBoxIntersection(float3 origin, float3 direction, float boxSize)
-		{
-			float nearestPoint = float.MaxValue;
-			for(int i = 0; i < 6; i++)
-			{
-				var faceNormal = BlockFacing.ALLFACES[i].Normalf;
-				float denom = math.dot(direction, new float3(faceNormal.X, faceNormal.Y, faceNormal.Z));
-				if(denom > 0f)
-				{
-					nearestPoint = Math.Min(nearestPoint, -(math.dot(origin, new float3(faceNormal.X, faceNormal.Y, faceNormal.Z)) + boxSize) / denom);
-				}
-			}
-			return origin + direction * nearestPoint;
-		}
-
-		private static void EnumerateLine(int gx0, int gy0, int gz0, int gx1, int gy1, int gz1, Action<int, int, int> visitor)
-		{
-			var sx = gx1 > gx0 ? 1 : (gx1 < gx0 ? -1 : 0);
-			var sy = gy1 > gy0 ? 1 : (gy1 < gy0 ? -1 : 0);
-			var sz = gz1 > gz0 ? 1 : (gz1 < gz0 ? -1 : 0);
-
-			//Error is normalized to vx * vy * vz so we only have to multiply up
-			var gx = (gx1 == gx0 ? 1 : gx1 - gx0) * (gy1 == gy0 ? 1 : gy1 - gy0);
-			var gy = (gx1 == gx0 ? 1 : gx1 - gx0) * (gz1 == gz0 ? 1 : gz1 - gz0);
-			var gz = (gy1 == gy0 ? 1 : gy1 - gy0) * (gz1 == gz0 ? 1 : gz1 - gz0);
-
-			//Error from the next plane accumulators, scaled up by vx*vy*vz
-			// gx0 + vx * rx == gxp
-			// vx * rx == gxp - gx0
-			// rx == (gxp - gx0) / vx
-			var errx = gx1 > gx0 ? gz : 0;
-			var erry = gy1 > gy0 ? gy : 0;
-			var errz = gz1 > gz0 ? gx : 0;
-
-			var derrx = sx * gz;
-			var derry = sy * gy;
-			var derrz = sz * gx;
-
-			gx = gx0;
-			gy = gy0;
-			gz = gz0;
-
-			while(true)
-			{
-				visitor(gx, gy, gz);
-
-				if(gx == gx1 && gy == gy1 && gz == gz1) break;
-
-				//Which plane do we cross first?
-				var xr = Math.Abs(errx);
-				var yr = Math.Abs(erry);
-				var zr = Math.Abs(errz);
-
-				if(sx != 0 && (sy == 0 || xr < yr) && (sz == 0 || xr < zr))
-				{
-					gx += sx;
-					errx += derrx;
-				}
-				else if(sy != 0 && (sz == 0 || yr < zr))
-				{
-					gy += sy;
-					erry += derry;
-				}
-				else if(sz != 0)
-				{
-					gz += sz;
-					errz += derrz;
-				}
-			}
-		}
-
 		private class ChunkBatchGroup : IBatchDataBuilder<TVertex, TUniform>
 		{
 			[ThreadStatic]
@@ -227,24 +141,15 @@ namespace PowerOfMind.Systems.ChunkBatchers
 			{
 				if(blockLightUtil == null) blockLightUtil = new BlockLightUtil();
 
-				blockLightUtil.Init(batcher.blockAccessor, null, CommonExt.CreateLightUtil(batcher.worldAccessor));
+				blockLightUtil.Init(batcher.blockAccessor, null, CommonExt.CreateLightUtil(batcher.worldAccessor));//TODO: geometry util
 
-				int chunkSize = batcher.blockAccessor.ChunkSize;
 				foreach(var id in batcher.chunkStrings.GetNodeEnumerable(stringChain))
 				{
 					ref readonly var record = ref batcher.chunkStrings[id];
-					var builder = batcher.strings[record.stringId].builder;
-					var rayOrigin = record.fromOffset;
-					var rayDir = record.toOffset - rayOrigin;
-					EnumerateLine(record.fromBlock.x, record.fromBlock.y, record.fromBlock.z, record.toBlock.x, record.toBlock.y, record.toBlock.z, (x, y, z) => {
-						var localOffset = new int3(x, y, z) - index * chunkSize;
-						var p0 = FindRayBoxIntersection(rayOrigin - localOffset - 0.5f, rayDir, 0.5f) + 0.5f;
-						var p1 = FindRayBoxIntersection(rayOrigin + rayDir - localOffset - 0.5f, -rayDir, 0.5f) + 0.5f;
-						ref var unmanaged = ref blockLightUtil.unmanaged;
-						unmanaged.SetUpLightRGBs(blockLightUtil, new int3(x, y, z));
-						builder.Build(new int3(x, y, z), localOffset, p0, p1, ref unmanaged, context);
-					});
+					batcher.strings[record.stringId].builder.BuildChunk(index, batcher.blockAccessor, blockLightUtil, context);
 				}
+
+				blockLightUtil.Clear();
 			}
 
 			void IBatchDataBuilder<TVertex, TUniform>.GetDefaultData(out TVertex vertexDefault, out TUniform uniformsDefault)
@@ -258,20 +163,11 @@ namespace PowerOfMind.Systems.ChunkBatchers
 		{
 			public readonly int chunkId;
 			public readonly int stringId;
-			// Relative to chunk origin
-			public readonly float3 fromOffset;
-			public readonly float3 toOffset;
-			public readonly int3 fromBlock;
-			public readonly int3 toBlock;
 
-			public ChunkStringSegment(int chunkId, int stringId, float3 fromOffset, float3 toOffset, int3 fromBlock, int3 toBlock)
+			public ChunkStringSegment(int chunkId, int stringId)
 			{
 				this.chunkId = chunkId;
 				this.stringId = stringId;
-				this.fromOffset = fromOffset;
-				this.toOffset = toOffset;
-				this.fromBlock = fromBlock;
-				this.toBlock = toBlock;
 			}
 		}
 
@@ -288,8 +184,103 @@ namespace PowerOfMind.Systems.ChunkBatchers
 		}
 	}
 
+	public static class StringBatchUtil
+	{
+		public static void EnumerateLine(int3 p0, int3 p1, Action<int3> visitor)
+		{
+			var sx = p1.x > p0.x ? 1 : (p1.x < p0.x ? -1 : 0);
+			var sy = p1.y > p0.y ? 1 : (p1.y < p0.y ? -1 : 0);
+			var sz = p1.z > p0.z ? 1 : (p1.z < p0.z ? -1 : 0);
+
+			//Error is normalized to vx * vy * vz so we only have to multiply up
+			int3 g;
+			g.x = (p1.x == p0.x ? 1 : p1.x - p0.x) * (p1.y == p0.y ? 1 : p1.y - p0.y);
+			g.y = (p1.x == p0.x ? 1 : p1.x - p0.x) * (p1.z == p0.z ? 1 : p1.z - p0.z);
+			g.z = (p1.y == p0.y ? 1 : p1.y - p0.y) * (p1.z == p0.z ? 1 : p1.z - p0.z);
+
+			//Error from the next plane accumulators, scaled up by vx*vy*vz
+			// gx0 + vx * rx == gxp
+			// vx * rx == gxp - gx0
+			// rx == (gxp - gx0) / vx
+			var errx = p1.x > p0.x ? g.z : 0;
+			var erry = p1.y > p0.y ? g.y : 0;
+			var errz = p1.z > p0.z ? g.x : 0;
+
+			var derrx = sx * g.z;
+			var derry = sy * g.y;
+			var derrz = sz * g.x;
+
+			g.x = p0.x;
+			g.y = p0.y;
+			g.z = p0.z;
+
+			while(true)
+			{
+				visitor(g);
+
+				if(g.x == p1.x && g.y == p1.y && g.z == p1.z) break;
+
+				//Which plane do we cross first?
+				var xr = Math.Abs(errx);
+				var yr = Math.Abs(erry);
+				var zr = Math.Abs(errz);
+
+				if(sx != 0 && (sy == 0 || xr < yr) && (sz == 0 || xr < zr))
+				{
+					g.x += sx;
+					errx += derrx;
+				}
+				else if(sy != 0 && (sz == 0 || yr < zr))
+				{
+					g.y += sy;
+					erry += derry;
+				}
+				else if(sz != 0)
+				{
+					g.z += sz;
+					errz += derrz;
+				}
+			}
+		}
+
+		public static void RayCubeIntersection(float3 origin, float3 dir, float size, out float tmin, out float tmax)
+		{
+			tmin = float.NegativeInfinity;
+			tmax = float.PositiveInfinity;
+
+			float t0, t1;
+
+			if(dir.y != 0.0)
+			{
+				t0 = (-origin.y) / dir.y;
+				t1 = (size - origin.y) / dir.y;
+
+				tmin = Math.Max(tmin, Math.Min(t0, t1));
+				tmax = Math.Min(tmax, Math.Max(t0, t1));
+			}
+
+			if(dir.x != 0.0)
+			{
+				t0 = (-origin.x) / dir.x;
+				t1 = (size - origin.x) / dir.x;
+
+				tmin = Math.Max(tmin, Math.Min(t0, t1));
+				tmax = Math.Min(tmax, Math.Max(t0, t1));
+			}
+
+			if(dir.z != 0.0)
+			{
+				t0 = (-origin.z) / dir.z;
+				t1 = (size - origin.z) / dir.z;
+
+				tmin = Math.Max(tmin, Math.Min(t0, t1));
+				tmax = Math.Min(tmax, Math.Max(t0, t1));
+			}
+		}
+	}
+
 	public interface IStringBuilder
 	{
-		void Build(int3 blockPos, int3 chunkLocalPos, float3 fromOffset, float3 toOffset, ref BlockLightUtil.Unmanaged blockLight, IBatchBuildContext context);
+		void BuildChunk(int3 chunkIndex, IBlockAccessor blockAccessor, BlockLightUtil lightUtil, IBatchBuildContext batcher);
 	}
 }
