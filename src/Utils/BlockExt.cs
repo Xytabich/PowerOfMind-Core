c236f5c3a974e87b2062cb3f851a5f7a6436bc2b
Xytabich <xytabich@gmail.com>
2023-07-09 14:29:39 +0300

    block and item extensions

---
+++ src/Utils/BlockExt.cs
@@ -0,0 +1,45 @@
+﻿using Vintagestory.API.Common;
+
+namespace PowerOfMind.Utils
+{
+	public static class BlockExt
+	{
+		public static T BlockOrBehAs<T>(this Block block) where T : class
+		{
+			if(block == null) return null;
+			if(block is T inst) return inst;
+			return block.GetBehavior(typeof(T), true) as T;
+		}
+
+		public static bool BlockOrBehIs<T>(this Block block, out T inst) where T : class
+		{
+			if(block == null)
+			{
+				inst = null;
+				return false;
+			}
+
+			if((inst = block as T) != null) return true;
+			return (inst = block.GetBehavior(typeof(T), true) as T) != null;
+		}
+
+		public static T BlockEntityOrBehAs<T>(this BlockEntity be) where T : class
+		{
+			if(be == null) return null;
+			if(be is T inst) return inst;
+			return be.GetBehavior<T>();
+		}
+
+		public static bool BlockEntityOrBehIs<T>(this BlockEntity be, out T inst) where T : class
+		{
+			if(be == null)
+			{
+				inst = null;
+				return false;
+			}
+
+			if((inst = be as T) != null) return true;
+			return (inst = be.GetBehavior<T>()) != null;
+		}
+	}
+}
---
+++ src/Utils/ItemExt.cs
@@ -0,0 +1,26 @@
+﻿using Vintagestory.API.Common;
+
+namespace PowerOfMind.Utils
+{
+	public static class ItemExt
+	{
+		public static T ItemOrBehAs<T>(this CollectibleObject item) where T : class
+		{
+			if(item == null) return null;
+			if(item is T inst) return inst;
+			return item.GetCollectibleBehavior(typeof(T), true) as T;
+		}
+
+		public static bool ItemOrBehIs<T>(this CollectibleObject item, out T inst) where T : class
+		{
+			if(item == null)
+			{
+				inst = null;
+				return false;
+			}
+
+			if((inst = item as T) != null) return true;
+			return (inst = item.GetCollectibleBehavior(typeof(T), true) as T) != null;
+		}
+	}
+}
